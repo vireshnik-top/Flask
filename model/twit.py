22c0ab02cd90278c771f6739a90d3fa12897cdbc
vireshnik <vireshnik@mail.ru>
2024-01-09 11:47:49 +0300

    Ошибка устранена

--- model/twit.py
+++ model/twit.py
@@ -3,3 +3,9 @@ class Twit:
     def __init__(self, body: str, author: str):
         self.body = body
         self.author = author
+
+    def to_dict(self):
+        return {
+            'body': self.body,
+            'author': self.author
+        }
