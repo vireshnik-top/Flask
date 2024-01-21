2105e9dbaba27aae87efc6c8bf84259bc28f9cef
vireshnik <vireshnik@mail.ru>
2024-01-21 19:42:52 +0300

    Ошибка устранена

--- main.py
+++ main.py
@@ -8,22 +8,6 @@ twits = []
 app = Flask(__name__)
 
 
-# class CustomJSONEncoder(json.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, Twit):
-#             return {'body': obj.body, 'author': obj.author}
-#         else:
-#             return super().default(obj)
-#
-#
-# app.json_encoder = CustomJSONEncoder
-
-
-# @app.route('/ping', methods=['GET'])
-# def index():
-#     return jsonify({'response': 'Pong'})
-
-
 @app.route('/twit', methods=['POST'])
 def create_twit():
     if not request.json or not 'body' in request.json or not 'author' in request.json:
@@ -36,6 +20,8 @@ def create_twit():
 def read_twits():
     # Используйте метод to_dict для преобразования объектов Twit в словари
     return jsonify({'twits': [twit.to_dict() for twit in twits]})
+
+
 
 
 if __name__ == '__main__':
